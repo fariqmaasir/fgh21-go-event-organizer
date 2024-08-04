76dff391132bc9a4af2a7d58749147424aa0eb8c
fariqmaasir <mfariqmaasir@gmail.com>
2024-08-05 02:32:42 +0700

    feat: add validator to post and patch

--- main.go
+++ main.go
@@ -5,23 +5,22 @@ import (
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 type results struct {
 	Id       int    `json:"id"`
-	Name     string `json:"name" form:"name"`
-	Email    string `json:"email" form:"email"`
-	Password string `json:"-" form:"password"`
-}
-type account struct {
-	Email    string `json:"email" form:"email"`
-	Password string `json:"-" form:"password"`
+	Name     string `json:"name" form:"name" binding:"required"`
+	Email    string `json:"email" form:"email" binding:"required,email"`
+	Password string `json:"-" form:"password" validate:"required"`
 }
 type message struct {
 	Success bool        `json:"success"`
 	Message string      `json:"message"`
 	Results interface{} `json:"results,omitempty"`
 }
+
+var validate *validator.Validate
 
 func main() {
 	data :=
@@ -45,22 +44,35 @@ func main() {
 	})
 	r.POST("/users", func(c *gin.Context) {
 		user := results{}
-
 		c.Bind(&user)
+		if err := c.ShouldBind(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
 
-		user.Id = len(data) + 1
+		validate = validator.New()
 
-		data = append(data, user)
+		if err := validate.Struct(user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		} else {
+			user.Id = len(data) + 1
 
-		c.JSON(http.StatusOK,
-			message{
-				Success: true,
-				Message: "Create User success",
-				Results: user,
-			})
+			data = append(data, user)
+			c.JSON(http.StatusOK,
+				message{
+					Success: true,
+					Message: "Create User success",
+					Results: user,
+				})
+		}
+
+		// c.JSON(http.StatusOK, gin.H{"message": "User data is valid!"})
+		// c.Bind(&user)
+
 	})
 	r.POST("/auth/login", func(c *gin.Context) {
-		user := account{}
+		user := results{}
 		cont := false
 
 		c.Bind(&user)
@@ -111,25 +123,36 @@ func main() {
 		user := results{}
 
 		c.Bind(&user)
-
-		for i, x := range data {
-			if x.Id == id {
-				data[i].Name = user.Name
-				data[i].Email = user.Email
-				data[i].Password = user.Password
-				c.JSON(http.StatusOK, message{
-					Success: true,
-					Message: "Data Users Success",
-					Results: data,
-				})
-				return
-			}
+		if err := c.ShouldBind(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
 		}
 
-		c.JSON(http.StatusNotFound, message{
-			Success: false,
-			Message: "User Not Found",
-		})
+		validate = validator.New()
+
+		if err := validate.Struct(user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		} else {
+			for i, x := range data {
+				if x.Id == id {
+					data[i].Name = user.Name
+					data[i].Email = user.Email
+					data[i].Password = user.Password
+					c.JSON(http.StatusOK, message{
+						Success: true,
+						Message: "Data Users Success",
+						Results: data[i],
+					})
+					return
+				}
+			}
+
+			c.JSON(http.StatusNotFound, message{
+				Success: false,
+				Message: "User Not Found",
+			})
+		}
 	})
 	r.DELETE("/users/:id", func(c *gin.Context) {
 		id, _ := strconv.Atoi(c.Param("id"))
